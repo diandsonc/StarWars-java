f1337161dde725b00bc99646c66065a6e856c126
Diandson Cardoso <diandsonc@gmail.com>
2019-05-17 00:52:37 -0300

    Troca nome da tabela de planeta para idioma pt-br

--- src/main/java/com/starwars/api/models/Planet.java
+++ src/main/java/com/starwars/api/models/Planet.java
@@ -7,7 +7,7 @@ import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.index.TextIndexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document(collection = "planetas")
 public class Planet {
     @Id
     private String id;
