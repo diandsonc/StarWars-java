59bf49d24b11bec4e75870ad1bc41652fee32db1
Diandson Cardoso <diandsonc@gmail.com>
2019-05-17 03:09:41 -0300

    Alteracao do endpoint para v1

--- src/main/java/com/starwars/api/controllers/PlanetController.java
+++ src/main/java/com/starwars/api/controllers/PlanetController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/api/planets")
+@RequestMapping(path = "/v1/planets")
 public class PlanetController {
 
     @Autowired
